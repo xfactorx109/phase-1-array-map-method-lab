9e35109c4d62691ea38e9baf37852595cf566f23
Xavier Humes-Sanchez <xfactorx109@gmail.com>
2024-01-25 10:01:13 -0800

    passing test

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    return tutorial.split(' ').map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
+};
+
+console.log(titleCased());
